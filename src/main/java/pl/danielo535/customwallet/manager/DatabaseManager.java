e47c3a8bf015df5d1c7dcb00b5eb498454dbc878
danielo535 <79422069+danielo535@users.noreply.github.com>
2023-08-31 18:14:40 +0200

    add Manager

---
+++ src/main/java/pl/danielo535/customwallet/manager/DatabaseManager.java
@@ -0,0 +1,100 @@
+package pl.danielo535.customwallet.manager;
+
+import org.bukkit.plugin.Plugin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.bukkit.Bukkit.getLogger;
+
+/**
+ * Manages the connection to the MySQL database, creation of tables, and database operations.
+ */
+public class DatabaseManager {
+    public Connection connection;
+    private String mysql, sqlite;
+
+    private final Plugin plugin;
+    public DatabaseManager(Plugin plugin) {
+        this.plugin = plugin;
+    }
+    /**
+     * Establishes a connection to the MySQL database using configuration parameters.
+     */
+    public synchronized void connect(String type,String host,Integer port, String database,String username,String password) {
+        try {
+            if (type.equalsIgnoreCase("Mysql")) {
+                mysql = "jdbc:mysql://" + host + ":" + port + "/" + database;
+                connection = DriverManager.getConnection(mysql, username, password);
+            } else if (type.equalsIgnoreCase("SQlite")) {
+                sqlite = plugin.getDataFolder().getAbsolutePath() + "/wallet.db";
+                connection = DriverManager.getConnection("jdbc:sqlite:" + sqlite);
+            }
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+    }
+    /**
+     * Closes the connection to the MySQL database.
+     */
+    public synchronized void disconnect() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            handleSQLException(e);
+        } finally {
+            connection = null;
+        }
+    }
+    /**
+     * Creates necessary tables in the database if they don't exist.
+     *
+     * @throws SQLException If a database error occurs during table creation.
+     */
+    public void createTables() throws SQLException {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS wallet (" +
+                "player VARCHAR(64) NOT NULL," +
+                "money DOUBLE NOT NULL" +
+                ")";
+        try (PreparedStatement statement = connection.prepareStatement(createTableSQL)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+    }
+    /**
+     * Handles SQLExceptions by providing descriptive error messages based on SQL state codes.
+     *
+     * @param e The SQLException to handle.
+     */
+    public static void handleSQLException(SQLException e) {
+        String sqlState = e.getSQLState();
+        if (sqlState != null) {
+            if (sqlState.equals("08001")) {
+                getLogger().warning("✘ Unable to establish a database connection. Check the connection configuration.");
+            } else if (sqlState.equals("28000")) {
+                getLogger().warning("✘ User authentication error. Check the correctness of the username and password.");
+            } else if (sqlState.equals("42000")) {
+                getLogger().warning("✘ SQL syntax error. Check the correctness of the SQL query construction.");
+            } else if (sqlState.equals("08S01")) {
+                getLogger().warning("✘ Database connection error. There might be an issue with the database server.");
+            } else if (sqlState.equals("3D000")) {
+                getLogger().warning("✘ Database error. Check if the database exists.");
+            } else if (sqlState.equals("23000")) {
+                getLogger().warning("✘ This object already exists in the database.");
+            } else if (sqlState.equals("HY000")) {
+                getLogger().warning("✘ General database error. Contact the administrator.");
+            } else if (sqlState.equals("40001")) {
+                getLogger().warning("✘ Transaction error. Attempted an invalid operation on a transaction.");
+            } else if (sqlState.equals("42S02")) {
+                getLogger().warning("✘ Database error. Table does not exist in the database.");
+            } else {
+                getLogger().warning("✘ An unknown error occurred with SQL state code: " + sqlState);
+            }
+        }
+    }
+}
---
+++ src/main/java/pl/danielo535/customwallet/manager/WalletManager.java
@@ -0,0 +1,236 @@
+package pl.danielo535.customwallet.manager;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+import static pl.danielo535.customwallet.manager.DatabaseManager.handleSQLException;
+
+public class WalletManager {
+    private final DatabaseManager databaseManager;
+    public final Map<String, Double> walletCache = new HashMap<>();
+    public WalletManager(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
+    /**
+     * Adds a player to the database with a specified amount of money.
+     *
+     * @param player The player to be added to the database.
+     * @param integer The initial amount of money to allocate to the player.
+     */
+    public void addPlayerDatabase(Player player, Integer integer) {
+        String sql = "INSERT INTO wallet (player, money) VALUES (?, ?)";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setString(1, player.getName());
+            statement.setInt(2, integer);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+    }
+    /**
+     * Checks if a player is present in the database.
+     *
+     * @param player The player to check for in the database.
+     * @return True if the player is present in the database, false otherwise.
+     * @throws SQLException If a database error occurs during the operation.
+     */
+    public boolean checkPlayerDatabase(Player player) throws SQLException {
+        String sql = "SELECT * FROM wallet WHERE player = ?";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setString(1, player.getName());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return false;
+        }
+    }
+    /**
+     * Checks the amount of money in a player's wallet.
+     *
+     * @param player The player whose wallet balance is being checked.
+     * @return The amount of money in the player's wallet.
+     */
+    public double checkWalletMoney(Player player) {
+        String sql = "SELECT money FROM wallet WHERE player = ?";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setString(1, player.getName());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getDouble("money");
+                }
+            }
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+        return 0;
+    }
+
+    /**
+     * Adds an amount of money to a player's wallet.
+     *
+     * @param player The player whose wallet is being updated.
+     * @param number The amount of money to add.
+     * @return True if the wallet was successfully updated, false otherwise.
+     */
+    public boolean addWalletMoney(Player player, Double number) {
+        double currentBalance = walletCache.getOrDefault(player.getName(), checkWalletMoney(player));
+
+        // Updating cached balances
+        walletCache.put(player.getName(), currentBalance + number);
+
+        String sql = "UPDATE wallet SET money = ? WHERE player = ?";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setDouble(1, currentBalance + number);
+            statement.setString(2, player.getName());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return false;
+        }
+    }
+    /**
+     * Sets the amount of money in a player's wallet.
+     *
+     * @param player The player whose wallet balance is being set.
+     * @param number The new amount of money.
+     * @return True if the wallet was successfully updated, false otherwise.
+     */
+    public boolean setWalletMoney(Player player, Double number) {
+        String sql = "UPDATE wallet SET money = ? WHERE player = ?";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setDouble(1, number);
+            statement.setString(2, player.getName());
+            int rowsAffected = statement.executeUpdate();
+
+            // Updating cached balances
+            walletCache.put(player.getName(), number);
+
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return false;
+        }
+    }
+    /**
+     * Removes an amount of money from a player's wallet.
+     *
+     * @param player The player whose wallet is being updated.
+     * @param number The amount of money to remove.
+     * @return True if the wallet was successfully updated, false otherwise.
+     */
+    public boolean removeWalletMoney(Player player, Double number) {
+        double currentBalance = walletCache.getOrDefault(player.getName(), checkWalletMoney(player));
+        if (currentBalance < number) {
+            return false;
+        }
+        // Updating cached balances
+        walletCache.put(player.getName(), currentBalance - number);
+
+        String sql = "UPDATE wallet SET money = ? WHERE player = ?";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setDouble(1, currentBalance - number);
+            statement.setString(2, player.getName());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return false;
+        }
+    }
+    /**
+     * Pays a specific amount of money from one player's wallet to another.
+     *
+     * @param sender The sender of the command, acting as the payer.
+     * @param player The player receiving the payment.
+     * @param number The amount of money to be paid.
+     * @return True if the payment was successful, false otherwise.
+     */
+    public boolean payWalletMoney(CommandSender sender, Player player, Double number) {
+        double payerBalance = walletCache.getOrDefault(sender.getName(), checkWalletMoney((Player) sender));
+        if (payerBalance < number) {
+            return false;
+        }
+        double receiverBalance = walletCache.getOrDefault(player.getName(), checkWalletMoney(player));
+
+        // Updating cached balances
+        walletCache.put(sender.getName(), payerBalance - number);
+        walletCache.put(player.getName(), receiverBalance + number);
+
+        String sql = "UPDATE wallet SET money = ? WHERE player = ?";
+        try (PreparedStatement statement = databaseManager.connection.prepareStatement(sql)) {
+            statement.setDouble(1, payerBalance - number);
+            statement.setString(2, sender.getName());
+            statement.executeUpdate();
+
+            statement.setDouble(1, receiverBalance + number);
+            statement.setString(2, player.getName());
+            statement.executeUpdate();
+
+            return true;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            return false;
+        }
+    }
+    /**
+     * Formats the wallet money value based on the given amount.
+     * Values are formatted as K for thousands, M for millions, and B for billions.
+     *
+     * @param walletMoney The wallet money value to be formatted.
+     * @return The formatted wallet money value.
+     */
+    public static String formatWalletMoney(Double walletMoney) {
+        double moneyValue = walletMoney;
+
+        if (moneyValue < 1000 && moneyValue % 1 < 0.01) {
+            return roundingWalletMoney(moneyValue);
+        } else if (moneyValue < 1_000_000) {
+            double formattedValue = moneyValue < 1000 ? moneyValue : moneyValue / 1000.0;
+            String unit = moneyValue < 1000 ? "" : "K";
+            return formatFormattedValue(formattedValue, unit);
+        } else if (moneyValue < 1_000_000_000) {
+            double formattedValue = moneyValue / 1_000_000.0;
+            return formatFormattedValue(formattedValue, "M");
+        } else {
+            double formattedValue = moneyValue / 1_000_000_000.0;
+            return formatFormattedValue(formattedValue, "B");
+        }
+    }
+    /**
+     * Formats the given value and adds the specified unit to it.
+     *
+     * @param value The value to be formatted.
+     * @param unit  The unit to be added (e.g., K for thousands, M for millions, B for billions).
+     * @return The formatted value with the unit.
+     */
+    private static String formatFormattedValue(double value, String unit) {
+        String formattedValue = roundingWalletMoney(value);
+        return formattedValue + unit;
+    }
+    /**
+     * Rounds the wallet money value to two decimal places and removes trailing ".00".
+     *
+     * @param walletMoney The wallet money value to be rounded.
+     * @return The rounded wallet money value.
+     */
+    public static String roundingWalletMoney(double walletMoney) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String formattedValue = decimalFormat.format(walletMoney);
+        if (walletMoney == 0) {
+            return "0";
+        } else if (formattedValue.endsWith(".00")) {
+            formattedValue = formattedValue.substring(0, formattedValue.length() - 3);
+        }
+        return formattedValue;
+    }
+}
